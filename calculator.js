2f0d8ca12beb12f1e480d32f46282345354db784
okputadora <okputadora@gmail.com>
2016-11-19 21:11:34 -0500

    Clear button fucked everything up

--- calculator.js
+++ calculator.js
@@ -4,9 +4,11 @@ var opp = "";
 var memory = "";
 varLastId = "op";
 var operationComplete = false;
+var clearClicked = false;
 
 //number clicked
 function queu(id){
+
   console.log(operationComplete)
   if (operationComplete === true){
     // start over;
@@ -14,6 +16,7 @@ function queu(id){
     $("#operation").html("");
     $("#currentNumber").css("color", "whitesmoke");
     operationComplete = false;
+    operationSegment = "";
   }
   var num = $("#" + id).text();
   operationSegment += num;
@@ -36,6 +39,7 @@ function op(id){
     if (operationComplete === false){
       fullOperation.push(operationSegment);
     }
+
     fullOperation.push(opp);
     console.log(fullOperation);
     $("#operation").append(" " + opp + " ");
@@ -52,7 +56,14 @@ function divide(a,b){return a/b;}
 
 //button cliucks
 
- $(document).ready(function(){
+ $(window).on("load", function(){
+   $('.clear').each(function(index, element){
+
+     setTimeout(function(){
+       element.classList.remove('loading');
+     }, index * 200);
+
+   });
   $('.op').each(function(index, element){
 
     setTimeout(function(){
@@ -67,20 +78,48 @@ function divide(a,b){return a/b;}
     }, index * 200);
 
   });
+
   $(".num").click(function(){
     queu(this.id);
   });
 
   $(".op").click(function(){
+    clearClicked = false;
     op(this.id)
   });
 
   $("#ce").click(function(){
+    operationSegment = "";
+    console.log(lastId);
+    if (lastId === "num" && clearClicked === false){
+      fullOperation.push(operationSegment);
+      lastId = "op";
+    }
+    else if(lastId === "op"){
+      lastId = "num";
+    }
+    console.log(fullOperation);
     fullOperation.splice(-1);
-    $("").html(fullOperation[-1]);
-  })
+    console.log(lastId);
+    console.log(fullOperation);
+
+    // loop to create formatting with spaces
+    $("#operation").html("");
+    for (var i in fullOperation){
+      $("#operation").append(fullOperation[i]);
+      $("#operation").append(" ");
+    }
+
+    $("#currentNumber").html(fullOperation[fullOperation.length - 1]);
+    if (fullOperation.length === 0){
+      $("#currentNumber").html("0");
+    }
+    clearClicked = true;
+    // operationSegment = "";
+  });
 
   $("#ac").click(function(){
+    clearClicked = false;
     operationSegment = "";
     fullOperation = [];
     $("#operation").html("");
@@ -89,13 +128,14 @@ function divide(a,b){return a/b;}
 
   // solve
   $("#enter").click(function(){
+    clearClicked = false;
     fullOperation.push(operationSegment);
     console.log(fullOperation);
 
     while (fullOperation.length >= 3){
       //convert to int
-      fullOperation[0] = parseInt(fullOperation[0]);
-      fullOperation[2] = parseInt(fullOperation[2]);
+      fullOperation[0] = parseFloat(fullOperation[0]);
+      fullOperation[2] = parseFloat(fullOperation[2]);
       //choose function based on operation
       if (fullOperation[1] === "+"){
         var num = add(fullOperation[0], fullOperation[2])
@@ -115,7 +155,7 @@ function divide(a,b){return a/b;}
     fullOperation[0] = fullOperation[0].toString();
     $("#currentNumber").html(fullOperation);
     $("#currentNumber").css("color", "darksalmon");
-    operationSegment = "";
+    lastId = "num";
     operationComplete = true;
     console.log(fullOperation)
   })
